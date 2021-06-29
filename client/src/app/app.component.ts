17d96daab977d06c4ab3d27c083cce19be22b330
David <ddelval390@gmail.com>
2021-06-29 18:58:33 -0500

    Implemented front end communication with backend to register and sign in users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +12,20 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private accountService: AccountService
+  ) {}
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http
-    .get('https://localhost:5001/api/user')
-    .subscribe((response) => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    });
+  setCurrentUser() {
+    const userItem = localStorage.getItem('user');
+    let user: User | null = null;
+    
+    if(userItem) user = JSON.parse(userItem)
+    
+    this.accountService.setCurrentUser(user);
   }
 }
