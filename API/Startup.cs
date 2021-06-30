7c2d71a48508edacb76d82c42a1a8d17fd59c7f3
David <ddelval390@gmail.com>
2021-06-30 12:43:14 -0500

    implemented AutoMapper

--- API/Startup.cs
+++ API/Startup.cs
@@ -51,11 +51,11 @@ namespace API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
            
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            // }
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
 
             app.UseHttpsRedirection();
 
