a073c5f2d96414921ebbf2f455e32078ba95e8cc
David <ddelval390@gmail.com>
2021-07-01 19:58:21 -0500

    implemented form validation

--- API/Helper/AutoMapperProfiles.cs
+++ API/Helper/AutoMapperProfiles.cs
@@ -17,6 +17,7 @@ namespace API.Helper
                     src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
+            CreateMap<RegisterDto, AppUser>();
         }
     }
 }
